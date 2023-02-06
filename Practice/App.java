5be95367ede90d7700a2954a5fb6132dc78ec623
Robbie Christian Emmanuel Espaldon <100555644+itsmeyessir@users.noreply.github.com>
2023-02-06 10:33:02 +0800

    Update App.java

--- Practice/App.java
+++ Practice/App.java
@@ -10,12 +10,12 @@ public class App {
 		gender = g;
 		weight = w;
 		numOfJowa = noj;
-		System.out.println("Name: " + n + "\nGender: " + g + "\nWeight: " + w + "\nNumber of Jowa: " + noj);
+		System.out.println("Name: " + n + "\nGender: " + g + "\nWeight: " + w + "\nNumber of Jowa: " + noj + "\n");
 	}
 	
 	public static void main (String[] args) {
-		App x = new App("Alex", "Male", 66.8, 2);
-		App y = new App("David", "Male", 78, 8);
-		App z = new App("Robbie", "Male", 60, 0);
+		new App("Alex", "Male", 66.8, 2);
+		new App("David", "Male", 78, 8);
+		new App("Robbie", "Male", 60, 0);
 	}
 } 
